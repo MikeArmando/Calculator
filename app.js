279e6cb7c27775ab8ea1896643cc7e22dc2e9a7c
Mike Armando <mike.armando.m@gmail.com>
2022-02-03 14:31:15 -0800

    Update

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const screenNum = document.getElementById('screen-num')
 const allClear = document.getElementById('all-clear')
+const del = document.getElementById('del')
+
 let num = document.getElementById('num')
 
 allClear.addEventListener('click', function () {
