b315561392d0f9990b14a2e20646af5717f5d867
Mike Armando <mike.armando.m@gmail.com>
2022-02-03 17:28:56 -0800

    Update

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const screenNum = document.getElementById('screen-num')
 const allClear = document.getElementById('all-clear')
 const del = document.getElementById('del')
+const equals = document.getElementById('equals') 
+const plus = document.getElementById('plus')
 
-let num = document.getElementById('num')
 
 allClear.addEventListener('click', function () {
     screenNum.innerText = ''
