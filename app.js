1b2c033656fcac15a8f6dca0ad596162b20cc700
Mike Armando <mike.armando.m@gmail.com>
2022-02-05 19:48:24 -0800

    Update

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
 const numberBtn = document.querySelectorAll('[data-number]')
 const operationBtn = document.querySelectorAll('[data-operation]')
 const allClear = document.querySelectorAll('[data-equals]')
-
-
