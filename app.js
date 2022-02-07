42abe246c2cef04875fb9bd0e771c3a653dc647b
Mike Armando <mike.armando.m@gmail.com>
2022-02-06 17:32:19 -0800

    Update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const operationBtn = document.querySelectorAll('[data-operation]')
 const equals = document.querySelectorAll('[data-equals]')
 const allClear = document.querySelector('[data-all-clear]')
 const del = document.querySelector('[data-del]')
-
+const opBtn = document.querySelectorAll('[data-operation]')
 
 
 
