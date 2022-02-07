177fbc35b3f83ba4368fde9bb730a46063757a68
Mike Armando <mike.armando.m@gmail.com>
2022-02-06 16:59:39 -0800

    Update

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+const previousScreen = document.querySelector('[data-previous-screen]')
+const currentScreen = document.querySelector('[data-current-screen]')
+
 const numberBtn = document.querySelectorAll('[data-number]')
 const operationBtn = document.querySelectorAll('[data-operation]')
-const allClear = document.querySelectorAll('[data-equals]')
+const equals = document.querySelectorAll('[data-equals]')
+const allClear = document.querySelector('[data-all-clear]')
+const del = document.querySelector('[data-del]')
+
+
+
+
+
+
