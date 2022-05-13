8fb5028a0640acab5bb7a54ac1aaed04ec9a8193
Mike Armando <mike.armando.m@gmail.com>
2022-05-12 20:10:38 -0700

    Update

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const currentScreen = document.querySelector('[data-current-screen]')
 
 const numberBtn = document.querySelectorAll('[data-number]')
 const operationBtn = document.querySelectorAll('[data-operation]')
-const equals = document.querySelectorAll('[data-equals]')
+const equals = document.querySelector('[data-equals]')
 const allClear = document.querySelector('[data-all-clear]')
 const del = document.querySelector('[data-del]')
-const opBtn = document.querySelectorAll('[data-operation]')
 
 
 allClear.addEventListener('click', function () {
@@ -14,3 +13,30 @@ allClear.addEventListener('click', function () {
     currentScreen.innerHTML = ''
 })
 
+class Calculator {
+    constructor(previousScreen, currentScreen) {
+        this.previousScreen = previousScreen
+        this.currentScreen = currentScreen
+        this.clear()
+}
+
+clear() {
+    this.currentScreen = ''
+    this.previousScreen = ''
+    this.operation = undefined
+}
+
+delete() {
+}
+
+appendNumber(number) {
+}
+
+chooseOperation(operation) {
+}
+
+compute() {
+}
+
+updateDisplay() {
+}
